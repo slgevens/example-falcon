72ad83134c55445f200e2bcfbd22826ce1f80883
Evens SOLIGNAC <evens.solignac@besedo.com>
2023-08-07 12:00:04 +0200

    [EXAMPLE] Adds redis connection

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+    "context"
     "fmt"
     "net/http"
+    "github.com/go-redis/redis/v8"
 )
 
+var rdb *redis.Client
+var ctx = context.Background()
+
+func init() {
+    rdb = redis.NewClient(&redis.Options{
+        Addr: "localhost:6379", // Redis server address
+    })
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello, my name is Falcon")
+    incr := rdb.Incr(ctx, "counter")
+    count, err := incr.Result()
+    if err != nil {
+        http.Error(w, "Error incrementing counter", http.StatusInternalServerError)
+        return
+    }
+    fmt.Fprintf(w, "hello, my name is Falcon. You've visited %d times!", count)
 }
 
 func main() {
